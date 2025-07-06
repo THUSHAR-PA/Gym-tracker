18a1ac97c2b3fd2c3bfa4df2da69cc6bc9dd1362
shadow <thusharpa@gmail.com>
2025-07-06 10:44:50 +0000

    integrated docker

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-           input: [
-  'resources/css/style.css',
-  'resources/css/home.css',
-  'resources/js/app.js',
-],
+            input: [
+                'resources/css/style.css',
+                'resources/css/home.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
-        tailwindcss(),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
